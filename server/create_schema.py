e813577dc563ef892b6532af7baf8f834188f988
Sebastian Mancke <s.mancke@tarent.de>
2013-05-07 20:25:30 +0200

    changed category name to tooling

--- server/create_schema.py
+++ server/create_schema.py
@@ -60,14 +60,14 @@ db.execute("""INSERT INTO ratingitem (id, name, description, category, creation_
      (NULL, "ScalaX6", "Die Scala Sprache f&uuml;r die JVM", "prog", "smanck", """+db.time_now()+"""),
      (NULL, "ScalaX7", "Die Scala Sprache f&uuml;r die JVM", "prog", "smanck", """+db.time_now()+"""),
      (NULL, "Cucumber", "Semantisches Testen mit Cucumber", "test", "smanck", """+db.time_now()+"""),
-     (NULL, "Chef", "Chef, das Tool zur Systemautomatisierung", "infra", "smanck", """+db.time_now()+"""),
-     (NULL, "Puppet", "Puppet, das Tool zur Systemautomatisierung", "infra", "smanck", """+db.time_now()+"""),
+     (NULL, "Chef", "Chef, das Tool zur Systemautomatisierung", "tooling", "smanck", """+db.time_now()+"""),
+     (NULL, "Puppet", "Puppet, das Tool zur Systemautomatisierung", "tooling", "smanck", """+db.time_now()+"""),
      (NULL, "Java Portlet", "Portale nach em Java Portlet Standard (z.B. Liferay und Co)", "paradigm", "smanck", """+db.time_now()+""");""")
 
 
 db.execute("""INSERT INTO category (id, name, description, orderindex) VALUES 
                 ("prog", "Programmiersprachen & Frameworks", "In diese Kategorie geh&ouml;ren alle Programmiersprachen sowie die zugeh&ouml;rigen wichtigen Bibliotheken und Frameworks", 1),
-                ("infra", "Infrastruktur & Tooling", "In diese Kategorie geh&ouml;ren alle Aspekte zum Management von Infrastruktur, Automatisierung, CI/CD. Sowie Toolchain Themen z.B. maven.", 2),
+                ("tooling", "Tooling", "In diese Kategorie geh&ouml;ren alle Aspekte zum Management von Infrastruktur, Automatisierung, CI/CD. Sowie Toolchain Themen z.B. maven.", 2),
                 ("test", "Testing", "In diese Kategorie geh&ouml;rt alles zum Testen von Software. Dies kann sowohl Technologie, als auch Test-Tooling sein.", 3),
                 ("paradigm", "Architektur, Paradigmen, Vorgehen", "In dieser Kategorie sind grunds&auml;tzliche Vorgehensmuster und Prinzipien anzusiedeln. Dies k&ouml;nnen technische, aber auch reine Prozessthemen sein.", 4);""")
 
